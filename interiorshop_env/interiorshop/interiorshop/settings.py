ea1509ec2477624eb43d6373f911f9e8e8d4bb6e
Paul Browne <paulbrowne2@gmail.com>
2021-06-16 20:16:28 +0100

    Created .env to keep environment variables secure

--- interiorshop_env/interiorshop/interiorshop/settings.py
+++ interiorshop_env/interiorshop/interiorshop/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c9p#p#)v4=+2pv2iujsu8!=&qc!o69)$67m&nr!o0hdbzl4---'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
