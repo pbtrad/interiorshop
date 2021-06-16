2bea1177d07a655583828ede3f7c4f4d0dd5fc73
Paul Browne <paulbrowne2@gmail.com>
2021-06-16 23:02:13 +0100

    Added login template with form

--- interiorshop_env/interiorshop/apps/vendor/urls.py
+++ interiorshop_env/interiorshop/apps/vendor/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
     path('become-vendor/', views.become_vendor, name='become_vendor'),
     path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login'),
 ]
