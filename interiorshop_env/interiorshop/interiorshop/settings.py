3bb19ee777a08ba11ed7ffa61118d3eaf3d727d6
paul <paulbrowne2@gmail.com>
2022-03-20 07:43:10 +0000

    Installed packages for heroku deployment, along with Procfile

--- interiorshop_env/interiorshop/interiorshop/settings.py
+++ interiorshop_env/interiorshop/interiorshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 import environ
 
@@ -102,12 +103,17 @@ WSGI_APPLICATION = 'interiorshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
